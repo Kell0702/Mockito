6e6e76b8f9c476daa6a19ca79bbc23908d9524ef
Yurii Trysov <87304775+Kell0702@users.noreply.github.com>
2021-10-25 10:39:51 +0300

    4 commit

--- src/main/java/ru/netology/Manager.java
+++ src/main/java/ru/netology/Manager.java
@@ -5,6 +5,7 @@ import ru.netology.repository.CartRepository;
 import java.util.Objects;
 
 public class Manager {
+    private static Object ListFilms;
     private CartRepository repository;
     private int length;
     private int listLength;
@@ -15,12 +16,16 @@ public class Manager {
         this.listLength = listLength;
     }
 
-    public CartRepository getRepository() {
-        return repository;
+    public static void Manager(String[] args) {
+        Manager repository = new Manager((CartRepository) ListFilms, 10, 8);
     }
 
     public void setRepository(CartRepository repository) {
         this.repository = repository;
+    }
+
+    public CartRepository getRepository() {
+        return repository;
     }
 
     public void add(ListFilms[] item) {
@@ -43,17 +48,25 @@ public class Manager {
 
     public ListFilms[] getAll() {
         ListFilms[] items = repository.findAll();
-//        int listFilms = items.length;
-//        if (items.length < length) {
-//        } else {
-//            listFilms = length;
-//        }
         ListFilms[] result = new ListFilms[length];
         for (int i = 0; i < result.length; i++) {
             int index = items.length - i - 1;
             result[i] = items[index];
         }
         return result;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Manager manager = (Manager) o;
+        return length == manager.length && listLength == manager.listLength && Objects.equals(repository, manager.repository);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(repository, length, listLength);
     }
 
 }
